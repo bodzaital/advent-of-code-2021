13763a9cb1aa018107ff2a2d4ab9afe8156222fe
Zsolt Boda <5047426+bodzaital@users.noreply.github.com>
2021-12-01 09:53:01 +0100

    Day 1

---
+++ aoc/Program.cs
@@ -0,0 +1,42 @@
+﻿// Day 1: Sonar Sweep
+
+// Part 1
+
+string[] lines = File.ReadAllLines("Day1.txt");
+int[] depths = lines.Select(x => int.Parse(x)).ToArray();
+int larger = 0;
+
+for (int i = 1; i < depths.Length; i++)
+{
+    if (depths[i] > depths[i - 1])
+    {
+        larger++;
+    }
+}
+
+Console.WriteLine($"{larger} measurements are larger than the previous measurement.");
+
+// Part 2
+
+List<int> sums = new();
+larger = 0;
+
+for (int i = 0; i < depths.Length; i++)
+{
+    if (i + 2 > depths.Length - 1)
+    {
+        break;
+    }
+
+    sums.Add(depths[i] + depths[i + 1] + depths[i + 2]);
+}
+
+for (int i = 1; i < sums.Count; i++)
+{
+    if (sums[i] > sums[i - 1])
+    {
+        larger++;
+    }
+}
+
+Console.WriteLine($"{larger} 3-window sums are larger than the previous 3-window sums.");
